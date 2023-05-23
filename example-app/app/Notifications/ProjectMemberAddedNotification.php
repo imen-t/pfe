c9888bb66a041cd63ff9056ec6fa5b43f72cb884
imen-t <torjmenimen01@gmail.com>
2023-05-23 01:14:46 +0100

    design

--- example-app/app/Notifications/ProjectMemberAddedNotification.php
+++ example-app/app/Notifications/ProjectMemberAddedNotification.php
@@ -7,17 +7,20 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use App\Http\Controllers\ProjectController;
+use App\Models\Project;
 
 class ProjectMemberAddedNotification extends Notification
 {
     use Queueable;
 
+    protected $project;
+
     /**
      * Create a new notification instance.
      */
-    public function __construct()
+    public function __construct(Project $project)
     {
-        //
+        $this->project = $project;
     }
 
     /**
@@ -25,7 +28,7 @@ class ProjectMemberAddedNotification extends Notification
      *
      * @return array<int, string>
      */
-    public function via(object $notifiable): array
+    public function via($notifiable)
     {
         return ['mail'];
     }
@@ -33,16 +36,18 @@ class ProjectMemberAddedNotification extends Notification
     /**
      * Get the mail representation of the notification.
      */
-    public function toMail(object $notifiable): MailMessage
+    public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->subject('You have been added to a project')
-                    ->from('barrett@example.com', 'Barrett Blair')
-                    ->line('You have been added to project ID :'. '.')
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/projects/templates/a3'))
-                    ->line('Thank you for using our application!');
+         
+            ->subject('You have been added to a project')
+            ->from('barrett@example.com', 'Barrett Blair')
+            ->line('You have been added to project ID: ' . $this->project->id)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/projects/templates/a3'))
+            ->line('Thank you for using our application!');
     }
+
 
     /**
      * Get the array representation of the notification.
--- example-app/resources/views/auth/register.blade.php
+++ example-app/resources/views/auth/register.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<div class="container" style="margin-top: 70px;">
+<section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+          class="img-fluid" alt="Sample image">
+      </div>
+      <div class="col-md-8 col-lg-4 col-xl-4 offset-xl-1">
+
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -73,5 +78,6 @@
             </div>
         </div>
     </div>
+</section>
 </div>
 @endsection
